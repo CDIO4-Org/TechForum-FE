bfb3b0c2d48c9bec1bf08672691363249706be46
NgBaoScript <hbkx22kx3@gmail.com>
2024-12-08 22:51:07 +0700

    update

--- src/app/modules/pages/components/components-routing.module.ts
+++ src/app/modules/pages/components/components-routing.module.ts
@@ -9,6 +9,7 @@ import { BlogBookmarkedComponent } from './user/blog-bookmarked/blog-bookmarked.
 import { BlogApproveComponent } from './admin/blog-approve/blog-approve.component';
 import { UserListComponent } from './admin/user-list/user-list.component';
 import { UserInfoComponent } from './user/user-info/user-info.component';
+import { ManageCategoryComponent } from './admin/manage-category/manage-category.component';
 
 
 
@@ -23,6 +24,7 @@ const routes: Routes = [
       { path: 'blog-approve', component: BlogApproveComponent },
       { path: 'user-list', component: UserListComponent },
       { path: 'info-personal', component: UserInfoComponent },
+      { path: 'manage-categories', component: ManageCategoryComponent },
     ]
   }
 ];
